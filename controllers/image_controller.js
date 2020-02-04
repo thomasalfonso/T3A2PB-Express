fce1a630844ee2edc84d55199933fd70a4db2cb2
mr-jameson <elliot.jameson4@gmail.com>
2020-02-04 17:10:10 +1100

    image upload to products working

--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -1,15 +1,19 @@
 const s3 = require('./../config/aws');
-const ImageModel = require("./../database/models/image_model");
-
+const ProductModel = require('./../database/models/product_model')
 //Create a multer instance and set the destination folder. The code below uses /public folder.
 
-const uploadImage = function(req, res, next) {
-  console.log(req.file);
+const uploadImage = async function(req, res, next) {
+  const {key} = req.file;
+  const {id} = req.body;
+  await ProductModel.findByIdAndUpdate(id, {image: key});
+  console.log(req.body)
+  console.log('id:', {id});
+  console.log('key: ', {key})
   res.send("");
 };
 
 const show = function(req, res) {
-  console.log(req.params);
+  // console.log(req.params);
   const { id } = req.params;
   const stream = s3.getObject({Key: id, Bucket:process.env.S3_BUCKET_NAME}).createReadStream()
   stream.pipe(res);
