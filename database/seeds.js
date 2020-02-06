0df506532aca1870b44365dba37b5c03bf5d2069
thomasalfonso <thomas.alfonso.email@gmail.com>
2020-02-06 15:22:30 +1100

    added db connection

--- database/seeds.js
+++ database/seeds.js
@@ -1,5 +1,6 @@
+require('./connect');
+require('dotenv').config({ path: './../.env' });
 const AdminModel = require('./models/admin_model');
-require('dotenv').config({ path: './../.env' })
 const email = process.env.ADMIN_EMAIL;
 const password = process.env.ADMIN_PASSWORD;
 AdminModel.create({email, password}).then(console.log('Admin creation')).catch((err)=>{console.log(err)})
